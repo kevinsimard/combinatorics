f7fc753c121596171d138efa378e72a8192f1706
Kevin Simard <kev.simard@gmail.com>
2016-11-10 12:41:16 -0500

    Replace single quotes by double quotes

--- README.md
+++ README.md
@@ -7,18 +7,18 @@
 You may add new elements to the list by using the `add` method:
 
 ```php
-$instance = new Combinatorics(['foo', 'bar']);
+$instance = new Combinatorics(["foo", "bar"]);
 
-$instance->add('baz');
-$instance->add('qux');
+$instance->add("baz");
+$instance->add("qux");
 
-// ['foo', 'bar', 'baz', 'qux']
+// ["foo", "bar", "baz", "qux"]
 ```
 
 You may also want to reset the list of elements by calling the `reset` method:
 
 ```php
-$instance = new Combinatorics(['foo', 'bar']);
+$instance = new Combinatorics(["foo", "bar"]);
 
 $instance->reset();
 
@@ -29,7 +29,7 @@ $instance->reset();
 
 ```php
 
-$elements = ['foo', 'bar', 'baz'];
+$elements = ["foo", "bar", "baz"];
 
 $instance = new Combinatorics($elements);
 foreach ($instance->permutations() as $value) {
@@ -43,12 +43,12 @@ foreach (Combinatorics::permutations($elements) as $value) {
 }
 
 // [
-//     ['foo', 'bar', 'baz'],
-//     ['bar', 'foo', 'baz'],
-//     ['bar', 'baz', 'foo'],
-//     ['foo', 'baz', 'bar'],
-//     ['baz', 'foo', 'bar'],
-//     ['baz', 'bar', 'foo']
+//     ["foo", "bar", "baz"],
+//     ["bar", "foo", "baz"],
+//     ["bar", "baz", "foo"],
+//     ["foo", "baz", "bar"],
+//     ["baz", "foo", "bar"],
+//     ["baz", "bar", "foo"]
 // ]
 ```
 
--- tests/CombinatoricsTest.php
+++ tests/CombinatoricsTest.php
@@ -6,17 +6,17 @@ class CombinatoricsTest extends PHPUnit_Framework_TestCase
 {
     public function test_add_adds_element()
     {
-        $instance = new Combinatorics(['foo', 'bar']);
+        $instance = new Combinatorics(["foo", "bar"]);
 
-        $instance->add('baz');
-        $instance->add('qux');
+        $instance->add("baz");
+        $instance->add("qux");
 
-        $this->assertEquals(['foo', 'bar', 'baz', 'qux'], $instance->elements());
+        $this->assertEquals(["foo", "bar", "baz", "qux"], $instance->elements());
     }
 
     public function test_reset_resets_elements()
     {
-        $instance = new Combinatorics(['foo', 'bar']);
+        $instance = new Combinatorics(["foo", "bar"]);
 
         $instance->reset();
 
@@ -25,9 +25,9 @@ class CombinatoricsTest extends PHPUnit_Framework_TestCase
 
     public function test_getter_returns_elements()
     {
-        $instance = new Combinatorics(['foo', 'bar']);
+        $instance = new Combinatorics(["foo", "bar"]);
 
-        $this->assertEquals(['foo', 'bar'], $instance->elements());
+        $this->assertEquals(["foo", "bar"], $instance->elements());
     }
 
     public function test_permutations_with_no_element()
@@ -42,41 +42,41 @@ class CombinatoricsTest extends PHPUnit_Framework_TestCase
 
     public function test_permutations_with_one_element()
     {
-        $instance = new Combinatorics(['foo']);
+        $instance = new Combinatorics(["foo"]);
 
         $generator = $instance->permutations();
         $values = iterator_to_array($generator);
 
-        $this->assertEquals([['foo']], $values);
+        $this->assertEquals([["foo"]], $values);
     }
 
     public function test_permutations_with_two_elements()
     {
-        $instance = new Combinatorics(['foo', 'bar']);
+        $instance = new Combinatorics(["foo", "bar"]);
 
         $generator = $instance->permutations();
         $values = iterator_to_array($generator);
 
         $this->assertEquals([
-            ['foo', 'bar'],
-            ['bar', 'foo'],
+            ["foo", "bar"],
+            ["bar", "foo"],
         ], $values);
     }
 
     public function test_permutations_with_three_elements()
     {
-        $instance = new Combinatorics(['foo', 'bar', 'baz']);
+        $instance = new Combinatorics(["foo", "bar", "baz"]);
 
         $generator = $instance->permutations();
         $values = iterator_to_array($generator);
 
         $this->assertEquals([
-            ['foo', 'bar', 'baz'],
-            ['bar', 'foo', 'baz'],
-            ['bar', 'baz', 'foo'],
-            ['foo', 'baz', 'bar'],
-            ['baz', 'foo', 'bar'],
-            ['baz', 'bar', 'foo'],
+            ["foo", "bar", "baz"],
+            ["bar", "foo", "baz"],
+            ["bar", "baz", "foo"],
+            ["foo", "baz", "bar"],
+            ["baz", "foo", "bar"],
+            ["baz", "bar", "foo"],
         ], $values);
     }
 }
